2ff5ce63e96469a46664107e4b2f29aa24e60788
Tetiana Novikova <nowikowa.tanya@gmail.com>
2018-01-16 11:19:07 -0700

    Add classes with comments

---
+++ FirstGame/Assets/Class2.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// class is an object classification
+// I must have open and closed {} per object
+public class Class2 : MonoBehaviour {
+
+	// void is a function that is void or empty or null
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
