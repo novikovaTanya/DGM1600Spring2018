51b24d78afed88cebd0756e4c05eb79c2d77bdd9
Tetiana Novikova <nowikowa.tanya@gmail.com>
2018-01-23 11:16:33 -0700

    Add comments to the Animal class
    Conventions and syntax Assignment

--- FirstGame/Assets/Animal.cs
+++ FirstGame/Assets/Animal.cs
@@ -2,6 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Assignment - Conventions and syntax
+// 
+// `public` is an identifier that allows us to access the class/method/variable publicly, without any restictions
+// `class` is keyword for creating the class
+// after the `class` keyword the name of the class goes
+// classes can follow the inheritance (have a father classes), which in C# is specified by `:`
+// everything what's inside curly brakets identify the class
+// class can have methods and variables
+
+// by defining a function you have to specify the returning type of that function
+// type `void` means that the function doesn't return anything
+// you can pass arguments to the function
+
+// to call the function you have to write the function name and the brakets, passing the parameters(if any)
+
+// semicolon goes after each piece of code
+
 public class Animal : MonoBehaviour {
 
 	// Hey there!
