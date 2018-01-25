b0187fce77150524e8ee1d751b099ecf22b11f21
Tetiana Novikova <nowikowa.tanya@gmail.com>
2018-01-25 11:25:12 -0700

    Variables Assignment

--- FirstGame/Assets/Animal.cs
+++ FirstGame/Assets/Animal.cs
@@ -2,24 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-// Assignment - Conventions and syntax
-// 
-// `public` is an identifier that allows us to access the class/method/variable publicly, without any restictions
-// `class` is keyword for creating the class
-// after the `class` keyword the name of the class goes
-// classes can follow the inheritance (have a father classes), which in C# is specified by `:`
-// everything what's inside curly brakets identify the class
-// class can have methods and variables
+/* Assignment - Conventions and syntax
 
-// by defining a function you have to specify the returning type of that function
-// type `void` means that the function doesn't return anything
-// you can pass arguments to the function
+`public` is an identifier that allows us to access the class/method/variable publicly, without any restictions
+`class` is keyword for creating the class
+after the `class` keyword the name of the class goes
+classes can follow the inheritance (have a father classes), which in C# is specified by `:`
+everything what's inside curly brakets identify the class
+class can have methods and variables
 
-// to call the function you have to write the function name and the brakets, passing the parameters(if any)
+by defining a function you have to specify the returning type of that function
+type `void` means that the function doesn't return anything
+you can pass arguments to the function
 
-// semicolon goes after each piece of code
+to call the function you have to write the function name and the brakets, passing the parameters(if any)
+
+semicolon goes after each piece of code */
+
+/* Assignment - Variables
+Variable is something that can store data. Variables can change.
+They can have different types, like int (integer), string (an array of characters), float, bool, etc.
+You can define/create your own type.
+
+To declare a variable you have to specify its type and identifier
+*/ 
+
+
 
 public class Animal : MonoBehaviour {
+
+	// string, int, bool, float are data types
+	// Name, FoodCourt, Hungry, MyFloat, HasFur are variables' identifiers
+	public string Name;
+	public int FoodCount;
+	public bool Hungry;
+	public float MyFloat;
+	public bool HasFur;
 
 	// Hey there!
 	// This is a new comment
@@ -31,7 +49,16 @@ public class Animal : MonoBehaviour {
 	
 	void OnMouseDown()
 	{
-		print("Meow.");
+		// You can access the variable by its identifier
+		print("I have a " + Name);
+		print("It has this amount of food: " + FoodCount);
+		print("Is hungry? - " + Hungry);
+		print("Does it have fur? - " + HasFur);
+
+		// You can change the variable
+		Hungry = false;
+		FoodCount++;
+		Name += " :)";
 	}
 
 	// Update is called once per frame
