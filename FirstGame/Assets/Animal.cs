1b9a9bb1ec8aad956614eaedf28b520632c1ab07
Tetiana Novikova <nowikowa.tanya@gmail.com>
2018-01-30 15:52:40 -0700

    If-statement Assignment

--- FirstGame/Assets/Animal.cs
+++ FirstGame/Assets/Animal.cs
@@ -8,12 +8,40 @@ public class Animal : MonoBehaviour {
 
 	void OnMouseDown()
 	{
+
+		// If Statements Assignment:
+
+		/*
+			If-statement should contain an 'if' keyword, followed by the condition and the body of the if-statement.
+			If the condition is true, what's inside the if-statement will be executed.
+		 */
+
+		// "Open door." will be executed if the Key variable is true
+		// if (Key == true) is the same as if (Key)
+
 		if(Key) {
 			print("Open door.");
 		}
 
+		// Here are few more examples of if-statements
 		if(Hungry) {
 			print("Feed me, human!");
+		}
+
+		if(2 + 3 == 566) {
+			print("This line won't print");
+		}
+
+		if (!Hungry) {
+			print("My animal is not hungry!");
+		}
+
+		if (Hungry == false) {
+			print("My animal is not hungry!!!");
+		}
+
+		if(false) {
+			print("Will never get here.");
 		}
 	}
 }
