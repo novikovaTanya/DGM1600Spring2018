7c730d812b2db435d85c20b2b0cc31e39547fc07
Tetiana Novikova <nowikowa.tanya@gmail.com>
2018-02-13 11:18:49 -0700

    In-Class: Arrays and Foreach Loop

---
+++ FirstGame/Assets/Scripts/ArraysAndLoops.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArraysAndLoops : MonoBehaviour {
+
+
+	public Player[] Players;
+	public int[] HighScores;
+	public PowerUp[] PowerUps;
+
+	void Start () {
+		foreach (var player in Players)
+			print(player.playerName + " " + player.score + " " + player.MPs);
+		
+		foreach (var player in Players)
+			if (player.score > 1000)
+				print(player.playerName + "'s score is higher than 1000");
+	}
+}
---
+++ FirstGame/Assets/Scripts/Player.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Player : ScriptableObject {
+	public string playerName;
+	public int score;
+	public int MPs;
+}
