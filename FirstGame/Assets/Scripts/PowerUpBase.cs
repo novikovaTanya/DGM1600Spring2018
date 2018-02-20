25685285d44fc5569496ed5fb76fdc2fbf38f75f
Tetiana Novikova <nowikowa.tanya@gmail.com>
2018-02-20 11:18:29 -0700

    Switch statements

---
+++ FirstGame/Assets/Scripts/PowerUpBase.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class PowerUpBase : ScriptableObject {
+	
+	public int PowerLevel;
+
+}
---
+++ FirstGame/Assets/Scripts/Switch.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/*
+	Switch Statement Assignment:
+
+	switch statement is like a set of if statements
+		syntax for a switch statement:
+			switch(expression) {
+				case constant-expression:
+					// do work
+					// break; (optional)
+				
+				// more cases
+				...
+
+				default:
+					// default case
+			}
+	switch statement can include any number of switch cases
+	when break statement is reached, the switch terminates
+
+	Below are few examples of using switch statement:
+*/
+
+
+public class Switch : MonoBehaviour {
+
+	public string UserName;
+	var MyPowerUpBase;
+
+	void OnMouseDown () {
+		switch (UserName) {
+			case "Password":
+				print("Password is correct");
+				break;
+			case "LevelUp":
+				print("You win! Next Level.");
+				break;
+			default:
+				print("Why don't you just tell me what to do?");
+				break;
+		};
+
+
+		// switch(MyPowerUpBase) {
+		// 	case AmmoPowerUp a:
+		// 		print("Ammo Power Up");
+		// 		break;
+		// 	case HealthPowerUp h:
+		// 		print("Health Power Up");
+		// 		break;
+		// 	default:
+		// 		print("No match...");
+		// 		break;
+		// }
+	}
+}
