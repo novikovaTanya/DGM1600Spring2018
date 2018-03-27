8a4265df4430daab7bc55851400454b9034a7ad7
Tetiana Novikova <nowikowa.tanya@gmail.com>
2018-03-27 11:20:55 -0600

    Create new Game project

---
+++ Game/Assets/Test/Scripts/Move.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    private Vector3 moveDirection = Vector3.zero;
+	private CharacterController controller;
+
+	private void Start() {
+		controller = GetComponent<CharacterController>();
+	}
+
+    void Update() {
+        if (controller.isGrounded) {
+			moveDirection.x = Input.GetAxis("Horizontal");
+			moveDirection.z = Input.GetAxis("Vertical");
+			moveDirection.y = 0;
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *= speed;
+            if (Input.GetButton("Jump"))
+                moveDirection.y = jumpSpeed;
+            
+        }
+        moveDirection.y -= gravity * Time.deltaTime;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+}
