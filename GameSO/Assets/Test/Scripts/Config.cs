5b02754c96b355a96c31853573a6069dcc27c9d0
Tetiana Novikova <nowikowa.tanya@gmail.com>
2018-03-29 01:50:02 -0600

    Work on Character Movements

---
+++ GameSO/Assets/Test/Scripts/Config.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Config : ScriptableObject {
+	public float speed;
+	public float jumpSpeed;
+	public float gravity;
+	public Vector3 intitialPosition = Vector3.zero;
+}
